f9dd2e607ed17599f8a3caac6255c8293458059c
James <jwblangley@gmail.com>
2018-06-25 18:13:37 +0100

    Begun change to JavaFX

--- src/FileCrypt.java
+++ src/FileCrypt.java
@@ -1,9 +1,5 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.Color;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,21 +7,30 @@ import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-public class FileCrypt {
+public class FileCrypt extends Application {
 
-  static JLabel statusLabel = new JLabel("READY");
-  static JPanel totalGUI = new JPanel();
-  static final int WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width,
-      HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+  static Label statusLabel = new Label("READY");
+  static final int WIDTH  = Toolkit.getDefaultToolkit().getScreenSize().width;
+  static final int HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+
 
   public static void main(String[] args) {
+
+    System.out.println(WIDTH);
+
+    launch(args);
+
+    /*
     JFrame.setDefaultLookAndFeelDecorated(true);
     JFrame frame = new JFrame("FileCrypt");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,20 +50,18 @@ public class FileCrypt {
 
     JButton encryptButton = new JButton("Calculate");
     encryptButton.setPreferredSize(new Dimension((int) (WIDTH * 0.25), (int) (HEIGHT * 0.05)));
-    encryptButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        if (!keyField.getText().equals("")) {
-          statusLabel.setText("WORKING...");
-          JFileChooser jfc = new JFileChooser();
-          int returnVal = jfc.showOpenDialog(null);
-          if (returnVal == JFileChooser.APPROVE_OPTION) {
-            statusLabel.setText((crypt(jfc.getSelectedFile()
-                .getAbsolutePath(), keyField.getText())) ? "DONE"
-                : "ERROR");
-          }
-        } else {
-          statusLabel.setText("EMPTY KEY");
+    encryptButton.addActionListener(e -> {
+      if (!keyField.getText().equals("")) {
+        statusLabel.setText("WORKING...");
+        JFileChooser jfc = new JFileChooser();
+        int returnVal = jfc.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+          statusLabel.setText((crypt(jfc.getSelectedFile()
+              .getAbsolutePath(), keyField.getText())) ? "DONE"
+              : "ERROR");
         }
+      } else {
+        statusLabel.setText("EMPTY KEY");
       }
     });
     totalGUI.add(encryptButton, BorderLayout.PAGE_END);
@@ -67,6 +70,8 @@ public class FileCrypt {
     frame.setVisible(true);
     frame.pack();
     frame.setLocationRelativeTo(null);
+
+    */
 
   }
 
@@ -123,4 +128,21 @@ public class FileCrypt {
     }
   }
 
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.setTitle("File Encryption");
+
+    BorderPane layout = new BorderPane();
+
+    statusLabel.setFont(Font.font("Verdana", HEIGHT / 30d));
+    layout.setBottom(statusLabel);
+    BorderPane.setAlignment(statusLabel, Pos.CENTER);
+
+    System.out.println(statusLabel.getWidth());
+
+    Scene scene = new Scene(layout, WIDTH * 0.3, HEIGHT * 0.2);
+    stage.setScene(scene);
+    stage.show();
+
+  }
 }
